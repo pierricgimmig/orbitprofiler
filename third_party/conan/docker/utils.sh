59a5a3379981b7b31c198a2880719669349c9fa8
Henning Becker <henning.becker@gmail.com>
2021-07-14 11:09:57 +0200

    fixup! Add function find_container_for_conan_profile

--- third_party/conan/docker/utils.sh
+++ third_party/conan/docker/utils.sh
@@ -3,22 +3,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Obtain current docker image tags and digests
-source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/tags.sh"
+# Obtain current docker image digests
 source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/digests.sh"
 
 function find_container_for_conan_profile {
-    readonly CONAN_PROFILE="$1"
-
-    # Use a specific tag from the mapping. If none is specified, fall back to `latest`.
-    readonly DOCKER_IMAGE_TAG="${docker_image_tag_mapping[${CONAN_PROFILE}]-latest}"
-
-    CONTAINER="gcr.io/orbitprofiler/${CONAN_PROFILE}:${DOCKER_IMAGE_TAG}"
-
-    if [ ${docker_image_digest_mapping[${CONAN_PROFILE}]+abc} ]; then
-        echo "Found a docker image digest. Using that to pin the container to an exact version." >&2
-        CONTAINER="${docker_image_digest_mapping[${CONAN_PROFILE}]}"
-    fi
-
-    echo "${CONTAINER}"
+    echo "${docker_image_digest_mapping[$1]}"
 }
